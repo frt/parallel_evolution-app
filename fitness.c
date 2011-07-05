e98b07d28c5d3a46d59ba38302065edc5a7be8cc
Fábio Roberto Teodoro <fr.teodoro@gmail.com>
2011-07-05 16:03:11 -0300

    rastrigin

--- fitness.c
+++ fitness.c
@@ -1,7 +1,19 @@
+#include <math.h>
+
+#define A 10	/* arbitrary constant, default for rastrigin */
+#define n 50	/* number of dimensions */
+
 /* define here the fitness function that will be used by all algorithms of parallel_evolution */
+/* fitness function for generalized rastrigin function */
 double fitness_func(double *x)
 {
-	return 0.0;
+	double sum = 0;
+	int i;
+
+	for (i = 0; i < n; ++i)
+		sum += x[i] * x[i] - A * cos(2 * M_PI * x[i]);
+
+	return A * n + sum;
 }
 
 double (*parallel_evolution_fitness_func)(double*) = fitness_func;
