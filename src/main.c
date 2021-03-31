fb8581c2be1d06a3968e4d6ae95513a5a3f2161e
Fábio Roberto Teodoro <fr.teodoro@gmail.com>
2021-03-31 20:16:36 -0300

    Topology now is read in the parallel_evolution-lib code.

--- src/main.c
+++ src/main.c
@@ -15,31 +15,8 @@ int main(int argc, char *argv[])
 {
 	algorithm_t *random_search_algorithm;
 	int ret;
-	topology_t *topology;
-    char *topology_file;
-    char topology_file_default[] = "ring.topology";
-
-    if (argc == 2)  // program name + args count
-        topology_file = argv[1];
-    else
-        topology_file = topology_file_default;
 
 	random_search_fitness_func = parallel_evolution_fitness_func;
-
-	/* create the topology */
-	if (topology_create(&topology) != SUCCESS) {
-		parallel_evolution_log(LOG_PRIORITY_ERR, MODULE_APP, "Topology could not be created. Quit.");
-		return ERROR_TOPOLOGY_CREATE;
-	}
-
-	/* parse topology from file */
-	if (topology_parser_parse(topology, topology_file) != SUCCESS) {
-		topology_destroy(&topology);
-		parallel_evolution_log(LOG_PRIORITY_ERR, MODULE_APP, "Topology could not be parsed. This is the end...");
-		return ERROR_TOPOLOGY_PARSE;
-	}
-
-	parallel_evolution_set_topology(topology);
 
 	parallel_evolution_set_number_of_dimensions(50);
 	algorithm_create(&random_search_algorithm,
@@ -56,7 +33,6 @@ int main(int argc, char *argv[])
 	ret = parallel_evolution_run(&argc, &argv);
 	
 	algorithm_destroy(&random_search_algorithm);
-	topology_destroy(&topology);
 	
 	return ret;
 }
